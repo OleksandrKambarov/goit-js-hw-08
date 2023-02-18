85a78947bc057476658908ef719d894c6441b2b6
OleksandrKambarov <113207904+OleksandrKambarov@users.noreply.github.com>
2023-02-18 12:14:46 +0200

    new Player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,13 @@
-import Player from '@vimeo/player';
-
 const iframe = document.querySelector('iframe');
-const player = new Player(iframe);
+const player = new Vimeo.Player(iframe);
 
 player.on('play', function () {
   console.log('played the video!');
 });
 
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
 // const onPlay = function (data) {
 //   duration: 61.857;
 //   percent: 0.049;
